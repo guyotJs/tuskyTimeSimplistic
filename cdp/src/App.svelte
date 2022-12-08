50d31546711556e79d041c0225f4e71ec181e264
ClassicMC <classicmclive@gmail.com>
2022-12-08 21:04:31 +0000

    s

--- cdp/src/App.svelte
+++ cdp/src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import tt from './tt.json';
+  import Header from './lib/Header.svelte';
   let active = false;
   function toggle(){
     if(active){active = false;}
@@ -22,6 +23,7 @@
       return year + "-" + (month+1) + "-" + day;
     }
   }
+  
   function select(input){
     day = input;
     dateString = compileTT();
@@ -29,32 +31,50 @@
   dateString = compileTT();
 </script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+h1{
+  font-size: 50px;
+}
+</style>
 <main>
-  <h1>Buskybime</h1>
+  <Header/>
+  <div class="options">
+    TODAY 
+  {#if tt[dateString] != undefined}
+    IS 
+    {#if tt[dateString].day == "A"}
+      AN
+      {:else}
+      A
+    {/if}<br/>
+    <h1>{tt[dateString].day}</h1>
+    DAY<br/>
+    AND TODAY IS TUSKY TIME<br/>
+    <h1>{tt[dateString].TT}</h1>
+    {:else}
+    THERE IS NO SCHOOL
+  {/if}
+  </div>
+
   <div class="custom-date">
     <div class="selected"  on:click={toggle}>
-      {dateString}&nbsp; <i class="fa fa-calendar" style="font-size:20px"></i>
+      &nbsp;<i class="fa fa-calendar" style="font-size:20px"></i>&nbsp;{dateString.replace(/-/g,'/')}
     </div>
-
     <div class="dates {active ? 'active' : ''}">
-
       <div class="month">
         <div class="arrows prev" on:click={prev}>&lt;</div>
         <div class="mth">{months[month]+ ' ' + year}</div>
         <div class="arrows next" on:click={post}>&gt;</div>
-
       </div>
       <div class="days">
         {#each Array(monthNum[month]) as _, i}
-            <div class="day" on:click={() => select(i+1)}>{(i+1).toString()}</div>
+            {#if day <= 9}
+              <div class="day {year+"-"+(month+1)+"-0"+(i+1) == dateString ? 'daySelect' : ''}" on:click={() => select(i+1)}>{(i+1).toString()}</div>
+              {:else}
+              <div class="day {year+"-"+(month+1)+"-"+(i+1) == dateString ? 'daySelect' : ''}" on:click={() => select(i+1)}>{(i+1).toString()}</div>
+            {/if}
         {/each}
       </div>
-      
     </div>
   </div>
-  {dateString}
-  {#if tt[dateString] != undefined }
-    {tt[dateString].TT}{:else}NO SCHOOL you absolute BUFFOON
-  {/if}
-  <br/>{day}
 </main>
---
+++ cdp/src/lib/Header.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+<style>
+    main{font-family: serif;}
+    h1{
+        text-align: center;
+        background-color: #6882d3;
+        padding: 10px;
+        margin-bottom: 50px;
+    }
+</style>
+<main>
+    <h1>TUSKY TIME</h1>
+</main>
